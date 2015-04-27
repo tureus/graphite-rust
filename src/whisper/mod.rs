24941f9f23999eb09aadd97aa89dc9063045a1f2
Xavier Lange <xrlange@gmail.com>
2015-04-27 00:08:22 -0700

    Editing docs

--- src/whisper/mod.rs
+++ src/whisper/mod.rs
@@ -1,3 +1,12 @@
+/*!
+
+All the code necessary for creating, reading, and updating
+whisper database files. The 'standard' for whisper files is defined
+primarily from the source code. It's a straight-forward format for
+static allocation of time-series
+
+*/
+
 extern crate byteorder;
 
 pub mod file;
