8562af9dc7261833903245938bae35ed8f176e2e
Xavier Lange <xrlange@gmail.com>
2015-09-18 15:39:57 -0700

    Shift to whisper mmap

--- src/whisper/file/impls/mod.rs
+++
@@ -1,5 +0,0 @@
-mod ref_cell_whisper_file;
-mod mutex_whisper_file;
-
-pub use self::ref_cell_whisper_file::RefCellWhisperFile;
-pub use self::mutex_whisper_file::MutexWhisperFile;
--- src/whisper/file/write_op.rs
+++
@@ -1,7 +0,0 @@
-use std::io::SeekFrom;
-
-#[derive(PartialEq,Debug)]
-pub struct WriteOp {
-    pub seek: SeekFrom,
-    pub bytes: [u8; 12]
-}
